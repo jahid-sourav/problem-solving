8a8f05120aaf0460febe04fe86f8a0961c36a3b5
jahid-sourav <info.jahidsourav@gmail.com>
2024-01-28 10:43:49 +0600

    Array Mapping Problem Soved

---
+++ 01.array/problem-06/app.js
@@ -0,0 +1,6 @@
+const numbers = [1, 2, 3, 4, 5];
+const SquareElement = numbers.map((number) => number * number);
+// const SquareElement = numbers.map(function(number){
+//    return number * number;
+// });
+console.log(SquareElement);
