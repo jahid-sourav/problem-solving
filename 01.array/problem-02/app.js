b4ae1912d428fbef3868cb77a00a75ddf2348614
jahid-sourav <info.jahidsourav@gmail.com>
2024-01-28 09:15:52 +0600

    Find Maximum Element Problem Solved

---
+++ 01.array/problem-02/app.js
@@ -0,0 +1,7 @@
+const numbers = [10, 20, 40, 60, 100];
+const maxValue = Math.max(...numbers);
+console.log(maxValue);
+/*
+Note -
+The spread operator (...) in JavaScript is used to expand elements of an array.
+*/ 
